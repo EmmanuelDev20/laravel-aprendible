66e2bcc5ef8ea735d81b7d5d2497974993f0df91
EmmanuelDev20 <emanuelovares@gmail.com>
2022-02-21 13:42:55 -0600

    Controladores

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PortfolioController extends Controller
+{
+  public function __invoke(Request $request)
+  {
+    $portfolio = [
+      ['title' => 'Proyecto 1'],
+      ['title' => 'Proyecto 2'],
+      ['title' => 'Proyecto 3'],
+      ['title' => 'Proyecto 4'],
+    ];
+
+    return view('portfolio', compact('portfolio'));
+  }
+}
